5b466d3370ebbb0183f7b2525cfb32cd9304c54a
Lara Tenkerian <laratenkerian@Laras-MacBook-Air.local>
2026-01-06 13:01:47 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/PriobulanceApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriobulanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriobulanceApplication.class, args);
+
+
+
+	}
+
+}
