20f90a0073070018910dc0f74ebf8dd25129b346
laratenkerian <tenkerianalid@gmail.com>
2026-01-11 16:12:22 -0500

    added patient entity

---
+++ src/main/java/com/example/demo/model/Patient.java
@@ -0,0 +1,34 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="patients")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+
+    //This creates a unique identifier for each row in database
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false )//this means every patient has to be full ( not empty)
+    private String firstName;
+    @Column(nullable = false )
+    private String lastName;
+    @Column(nullable = false )
+    private long patientId;
+    @Column(nullable = false )
+    private int age;
+    @Column(nullable = false )
+    private boolean pregnancyFlag;
+
+
+
+}
