fa0d7484610424da8579f5563aad8dbab5812fd5
laratenkerian <tenkerianalid@gmail.com>
2026-01-11 17:24:48 -0500

    added repo fro patient

--- src/main/java/com/example/demo/repository/PatientRepository.java
+++ src/main/java/com/example/demo/repository/PatientRepository.java
@@ -1,4 +1,12 @@
 package com.example.demo.repository;
 
-public class PatientRepository {
+import com.example.demo.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByPatientId(Long patientId);
+    List<Patient> findByEmergencyCallId(Long emergencyCallId);
 }
