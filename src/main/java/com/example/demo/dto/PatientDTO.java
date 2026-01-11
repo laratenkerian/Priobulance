938b5b73a7135c7eb74de028251016beb9688fd4
laratenkerian <tenkerianalid@gmail.com>
2026-01-11 17:03:21 -0500

    added patient dto

--- src/main/java/com/example/demo/dto/PatientDTO.java
+++ src/main/java/com/example/demo/dto/PatientDTO.java
@@ -1,4 +1,22 @@
 package com.example.demo.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PatientDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private long patientId;
+    private int age;
+    private boolean pregnancyFlag;
+
 }
