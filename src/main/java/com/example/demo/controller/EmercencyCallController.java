15e00f6443e166087a618b0a3b734fa92d3d1543
laratenkerian <tenkerianalid@gmail.com>
2026-01-13 11:46:24 -0500

    started the controller

--- src/main/java/com/example/demo/controller/EmercencyCallController.java
+++ src/main/java/com/example/demo/controller/EmercencyCallController.java
@@ -1,4 +1,17 @@
 package com.example.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//Handles HTTP requests
+@RequestMapping("/api/emergency-calls")//sets a base URL
+@RequiredArgsConstructor
+
+
+
 public class EmercencyCallController {
+    //private final EmergencyCallService emergencyCallService;
+
+
 }
